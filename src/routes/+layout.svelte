7c9a39593e26dff1829d250c9996c91589b8c7cf
Josh Kropf <josh@slashdev.ca>
2025-01-20 22:53:20 -0500

    Initial boilerplate with sveltekit, tailwind, daisyui

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+   import "../app.css";
+
+   import { page } from '$app/state';
+   // page.url.pathname
+
+   let { children } = $props();
+</script>
+
+{@render children()}
+
+<div class="dock">
+   <a href="/" class="dock-active">
+      <span class="dock-label">Home</span>
+   </a>
+
+   <button>
+      <span class="dock-label">Add</span>
+   </button>
+
+   <button>
+      <span class="dock-label">History</span>
+   </button>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1 class="text-lg">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button class="btn">Testing</button>
+<input type="text" placeholder="Type here" class="input" />
