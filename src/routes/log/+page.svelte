8e163298f78b44b90da0b24bf03f315164258bbc
Josh Kropf <josh@slashdev.ca>
2025-01-22 00:39:47 -0500

    Placeholder for log entry page

---
+++ src/routes/log/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+   import { goto } from "$app/navigation";
+</script>
+
+<div class="navbar bg-base-100 shadow-sm">
+   <div class="navbar-start">
+      <button class="btn" onclick={() => goto('/')}>Cancel</button>
+   </div>
+
+   <div class="navbar-end">
+      <button class="btn">Save</button>
+   </div>
+</div>
