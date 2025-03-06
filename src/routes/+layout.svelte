6714d78b1a3bfb4c77ff6db8618a056af1e2ec23
Josh Kropf <josh@slashdev.ca>
2025-03-05 23:39:32 -0500

    Add route transition animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
    import '../app.css';
-   import Icon from '$lib/Icon.svelte';
 
    import { page } from '$app/state';
+   import Icon from '$lib/Icon.svelte';
+   import { fade } from 'svelte/transition';
 
    let { children } = $props();
 </script>
 
-{@render children()}
+<!-- https://joshcollinsworth.com/blog/sveltekit-page-transitions -->
+{#key page.url}
+   <div in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 100 }}>
+      {@render children()}
+   </div>
+{/key}
 
 <div class="dock" class:hidden={page.data.hideDock}>
    <a href="/" class:dock-active={page.url.pathname == '/'}>
