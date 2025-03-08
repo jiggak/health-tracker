92de248899027d8ab08ace0e6de0c6ce151e7290
Josh Kropf <josh@slashdev.ca>
2025-03-07 20:46:35 -0500

    Switch to range control for single option input

--- src/routes/log/SingleOption.svelte
+++ src/routes/log/SingleOption.svelte
@@ -7,22 +7,42 @@
       onValueChanged(val?: LogValue): void
    } = $props();
 
-   let selectedOption = $derived.by(() => {
-      const opt = options?.find(o => o.value == value)
-      return opt != null? opt.label : null;
-   });
+   const index = options.findIndex((o) => o.value == value);
+   let selected = $state(index < 0 ? 0 : index);
 </script>
 
-<p class="h-10 text-center">
-   {selectedOption}
-</p>
+<p class="h-10 text-center">{options[selected].label}</p>
 
-<div class="flex justify-evenly">
-   {#each options as option}
-      <input type="radio"
-         class="radio"
-         onchange={(_) => onValueChanged(option.value)}
-         value={option.value}
-         bind:group={value} />
-   {/each}
+<div class="flex flex-col">
+   <input type="range" min="0" max={options.length - 1} step="1"
+      bind:value={selected}
+      onchange={() => onValueChanged(options[selected].value)}
+      class="range range-primary [--range-fill:0] w-full" />
+   <div class="dots">
+      {#each options as _, index}
+         <span class:selected={index == selected}></span>
+      {/each}
+   </div>
 </div>
+
+<style>
+   .dots {
+      display: flex;
+      justify-content: space-between;
+      padding-inline: 6px;
+      pointer-events: none;
+      position: relative;
+      top: -17px;
+
+      & span {
+         width: 10px;
+         height: 10px;
+         background-color: var(--color-neutral);
+         border-radius: 50%;
+
+         &.selected {
+            visibility: hidden;
+         }
+      }
+   }
+</style>
