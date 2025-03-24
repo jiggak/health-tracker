ce4c156d03043b2e1afd20cc2e6d75783d00a036
Josh Kropf <josh@slashdev.ca>
2025-03-24 14:59:25 -0400

    Fix error when producing build output

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,9 @@ const config = {
       // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
       // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
       // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-      adapter: adapter(),
+      adapter: adapter({
+         fallback: 'index.html'
+      }),
 
       alias: {
          'db-impl': process.env.TAURI_ENV_ARCH ? 'src/lib/db-sql.ts' : 'src/lib/db-web.ts'
