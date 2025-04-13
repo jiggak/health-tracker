b6caa34c431dcbda53b2113323046af824b98729
Josh Kropf <josh@slashdev.ca>
2025-04-13 01:50:17 -0400

    Add scalable android icon

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "io.kropf.health_tracker"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
